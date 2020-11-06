44cf8ae1e713f5472e53f862bd43a57ce1e56575
Humberto Garza <humberto.garza@ni.com>
2020-11-05 18:29:22 -0600

    update bootstarp to setup ELK 7 and configure the Logstash config folders and ymls

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,6 @@
 
 export DEBIAN_FRONTEND="noninteractive"
 
-ELK_VERSION="6.2.4"
-
 # update apt
 sudo apt-get update
 sudo apt-get DEBIAN_FRONTEND=noninteractive upgrade -y
@@ -15,11 +13,11 @@ echo $JAVA_HOME
 
 # install the Elastic PGP Key and repo
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 
 sudo apt-get update
 echo "[*] Installing Elastic Search"
-sudo apt-get install -y elasticsearch=$ELK_VERSION
+sudo apt-get install -y elasticsearch
 echo "[+] Done Installing Elastic Search"
 
 
@@ -31,10 +29,12 @@ sudo /bin/systemctl start elasticsearch.service
 
 # install Logstash
 echo "[*] Installing Logstash"
-sudo apt-get install -y logstash=1:$ELK_VERSION-1
+sudo apt-get install -y logstash
 
 # copy over configs
-cp -R /vagrant/configs/logstash/* /etc/logstash/conf.d/
+cp -R /vagrant/configs/logstash/conf.d/* /etc/logstash/conf.d/
+cp /vagrant/configs/logstash/logstash.yml /etc/logstash/
+cp /vagrant/configs/logstash/pipelines.yml /etc/logstash/
 sudo systemctl enable logstash.service
 sudo systemctl start logstash.service
 echo "[+] Done Installing Logstash"
@@ -42,7 +42,7 @@ echo "[+] Done Installing Logstash"
 
 # Install Kibana
 echo "[*] Installing Kibana"
-sudo apt-get install -y kibana=$ELK_VERSION
+sudo apt-get install -y kibana
 
 # copy over configs
 cp /vagrant/configs/kibana/kibana.yml /etc/kibana/
